e6d251d263466c04cdd6f90015ccf2fde910a5c5
Vignesh kumar <vigneshkumardandige@gmail.com>
2026-06-03 09:07:51 +0530

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,12 @@
+
+public class Product {
+
+    public Object category;
+    public int price;
+    public String productId;
+
+    public Product(String string, int i, int j) {
+        //TODO Auto-generated constructor stub
+    }
+
+}
